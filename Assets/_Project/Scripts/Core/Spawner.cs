0d437f34857ba7383c1e4890627c6c226cb6fee7
Senjucasie <paulkpraveen@gmail.com>
2023-05-04 17:20:50 +0200

    added spawn shape and get rand shape functions in spawner class added the prefabs to spawn in the inspector

--- Assets/_Project/Scripts/Core/Spawner.cs
+++ Assets/_Project/Scripts/Core/Spawner.cs
@@ -2,4 +2,26 @@
 
 public class Spawner:MonoBehaviour
 {
+    [SerializeField]private Shape[] _shapeArray;
+
+    private Shape GetRandomShape()
+    {
+        Shape shape = _shapeArray[Random.Range(0, _shapeArray.Length)];
+        if(shape==null)
+        {
+            Debug.LogWarning("Couldnt find the shape in the array to spawn");
+            return null;
+        }
+        else
+        {
+            return shape;
+        }
+    }
+
+    public Shape SpawnShape()
+    {
+        Shape shape = Instantiate(GetRandomShape(), transform.position, Quaternion.identity) as Shape;
+
+        return shape;
+    }
 }
