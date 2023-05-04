8985262fee9c07c7834b3557686230389d261da8
Senjucasie <paulkpraveen@gmail.com>
2023-05-04 16:54:44 +0200

    created spawner and game controller class

---
+++ Assets/_Project/Scripts/Manager/GameController.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    [SerializeField] private Board _board;
+    [SerializeField] private Spawner _spawner;
+    
+}
